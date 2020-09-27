cc35ca34e87baa7a25ae914f1df573664cba60ed
yeti_detective <yeti.detective@gmail.com>
2020-09-27 19:38:57 -0400

    readme and URL for heroku pushing

---
+++ README.md
@@ -0,0 +1,2 @@
+# this bizniz produced by following article here: https://blog.logrocket.com/creating-a-web-server-with-golang/
+# pushed to heroku following instructions here: https://devcenter.heroku.com/articles/getting-started-with-go
--- server.go
+++ server.go
@@ -1,4 +1,5 @@
 // this bizniz produced by following article here: https://blog.logrocket.com/creating-a-web-server-with-golang/
+// pushed to heroku following instructions here: https://devcenter.heroku.com/articles/getting-started-with-go
 package main
 
 import (
